af60008c79ea147e675b06ac9708ca00b0fef367
TuByuem <tubyuem@wp.pl>
2015-02-01 22:16:11 +0100

    Dodanie compilerPassa do komend

--- initContainer.php
+++ initContainer.php
@@ -1,9 +1,12 @@
 <?php
 
-use Symfony\Component\DependencyInjection;
+use Symfony\Bundle\FrameworkBundle\DependencyInjection\Compiler\AddConsoleCommandPass;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 $container = new DependencyInjection\ContainerBuilder();
 $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
 $loader->load('services.yml');
+
+$container->addCompilerPass(new AddConsoleCommandPass());
