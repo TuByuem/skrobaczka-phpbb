a2249777dca78c795037eac4e21878eaa4c30f77
TuByuem <tubyuem@wp.pl>
2015-02-01 22:31:20 +0100

    Dodanie nowej komendy konsolowej oraz autoload

---
+++ TuByuem/Skrobaczka/Command/Scrap.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TuByuem\Skrobaczka\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Description of Scrap
+ *
+ * @author TuByuem <tubyuem@wp.pl>
+ */
+class Scrap extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function run(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('test');
+    }
+}
