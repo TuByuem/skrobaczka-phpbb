c9e350dd4bc45a76ab2355b172752246e6d47485
TuByuem <tubyuem@wp.pl>
2015-02-01 22:45:23 +0100

    Ładowanie komend z DI Containera

--- application.php
+++ application.php
@@ -6,5 +6,11 @@ require_once './initContainer.php';
 use Symfony\Component\Console\Application;
 
 $application = new Application();
+/* @var $container Symfony\Component\DependencyInjection\Container */
+if ($container->hasParameter('console.command.ids')) {
+    foreach ($container->getParameter('console.command.ids') as $commandId) {
+        $application->add($container->get($commandId));
+    }
+}
 $application->run();
 
