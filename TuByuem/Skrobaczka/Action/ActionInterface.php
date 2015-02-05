cf4b1518b3dbd47157f5f282cb76db0afe22784c
TuByuem <tubyuem@wp.pl>
2015-02-05 10:04:22 +0100

    Zmiany w projekcie akcji

--- TuByuem/Skrobaczka/Action/ActionInterface.php
+++ TuByuem/Skrobaczka/Action/ActionInterface.php
@@ -2,13 +2,17 @@
 
 namespace TuByuem\Skrobaczka\Action;
 
+use Symfony\Component\DomCrawler\Crawler;
+use TuByuem\Skrobaczka\Exception\ActionNotReadyException;
+
 /**
  * @author TuByuem <tubyuem@wp.pl>
  */
 interface ActionInterface
 {
     /**
-     * @param array $options
+     * @return Crawler
+     * @throws ActionNotReadyException
      */
-    public function perform(array $options);
+    public function getCrawler();
 }
